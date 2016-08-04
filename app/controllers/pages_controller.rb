d17d06181228fcac6b8f1444feccfeb6815473b8
Edgar <etm9@jelpmi.mx>
2016-08-03 22:48:37 -0500

    final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   def x
   end
 
+  def save_form
+  	puts params[:q]
+  	puts params[:r]
+  	puts params[:s]
+  	Usuario.create(nombre: params[:q], email: params[:r], edad: params[:s])
+  	redirect_to pages_x_path, notice: "El usuario ha sido guardado"
+  end
   def index
   end
 
